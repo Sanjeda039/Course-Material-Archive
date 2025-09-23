e362df2a7e5de1a8743a634304a4f3e91832694d
Sanjeda039 <109533731+Sanjeda039@users.noreply.github.com>
2025-09-23 19:28:03 +0600

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,89 +1,75 @@
+// Navbar toggle
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
 
+// Scroll active link
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header .navbar a');
 
-window.onscroll = () =>{
-
+window.onscroll = () => {
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 
-  section.forEach(sec =>{
-
+  section.forEach(sec => {
     let top = window.scrollY;
     let height = sec.offsetHeight;
     let offset = sec.offsetTop - 150;
     let id = sec.getAttribute('id');
 
     if(top >= offset && top < offset + height){
-      navLinks.forEach(links =>{
-        links.classList.remove('active');
-        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
-      });
-    };
-
+      navLinks.forEach(links => links.classList.remove('active'));
+      document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+    }
   });
-
 }
 
-document.querySelector('#search-icon').onclick = () =>{
+// Search form toggle
+document.querySelector('#search-icon').onclick = () => {
   document.querySelector('#search-form').classList.toggle('active');
 }
 
-document.querySelector('#close').onclick = () =>{
+document.querySelector('#close').onclick = () => {
   document.querySelector('#search-form').classList.remove('active');
 }
 
+// Swiper home slider
 var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 7500,
+    delay: 3000, // you can adjust
     disableOnInteraction: false,
   },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  loop:true,
+  loop: true,
 });
 
-var swiper = new Swiper(".review-slider", {
+// Swiper review slider (if you have one)
+var swiper2 = new Swiper(".review-slider", {
   spaceBetween: 20,
   centeredSlides: true,
   autoplay: {
-    delay: 7500,
+    delay: 3000,
     disableOnInteraction: false,
   },
-  loop:true,
+  loop: true,
   breakpoints: {
-    0: {
-        slidesPerView: 1,
-    },
-    640: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
+    0: { slidesPerView: 1 },
+    640: { slidesPerView: 2 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
   },
 });
 
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
-}
-
-function fadeOut(){
-  setInterval(loader, 3000);
-}
-
-window.onload = fadeOut;
+// Loader
+window.addEventListener("load", () => {
+  document.querySelector('.loader-container').style.display = "none";
+});
